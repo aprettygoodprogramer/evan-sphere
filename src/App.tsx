dd0fd1ec2d5b1c1ade0cd7a7ad6bf92bf3ef0c92
Evan <evan.j.ducas@gmail.com>
2024-11-26 13:09:34 -0500

    added some epic animations :)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import ListGroup from './comp/ListGroup';
 import DropDown from './comp/dropdown';
-
-function App()
-{
-  return <div><ListGroup /> <DropDown/></div>
+import Text from './comp/text';
+import './app.css';
+function App() {
+  return (
+    <div className="page-background">
+      <Text
+        size="large"
+        weight="bold"
+        color="white"
+        className="h1"
+        fontFamily="Courier New"
+      >
+        Welcome To Evan-Sphere
+      </Text>
+    </div>
+  );
 }
 
 export default App;
