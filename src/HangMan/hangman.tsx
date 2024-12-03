7dbe2a9c10dd0bbb1485588fd42f96f0bae31590
Evan <evan.j.ducas@gmail.com>
2024-12-02 20:52:27 -0500

    added the images to hang man

--- src/HangMan/hangman.tsx
+++ src/HangMan/hangman.tsx
@@ -4,7 +4,12 @@ import { Outlet, Link } from "react-router-dom";
 import TextField from "./comp/singleLetterInput";
 import words from "./wordlist.json";
 import "../App.css";
-
+import hang1 from "./HangManPic/Hang1.png";
+import hang2 from "./HangManPic/Hang2.png";
+import hang3 from "./HangManPic/Hang3.png";
+import hang4 from "./HangManPic/Hang4.png";
+import hang5 from "./HangManPic/Hang5.png";
+import hang6 from "./HangManPic/Hang6.png";
 function GetRandomWord() {
   const randomIndex = Math.floor(Math.random() * words.length);
   return words[randomIndex];
@@ -25,7 +30,7 @@ function HangMan() {
       </Text>
 
       <TextField onSubmit={handleWordSubmit} />
-
+      <img src={hang1} alt="hang1" width={284} height={392} />
       <div className="button-container">
         <button className="github-button">
           <Link to="/">back to home page</Link>
