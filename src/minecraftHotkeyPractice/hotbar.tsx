94457db2f70f75df722c56a70f754607ec479e0b
Evan <evan.j.ducas@gmail.com>
2025-02-28 09:27:26 -0500

    Refactor hotkey components and styles for improved layout and functionality

---
+++ src/minecraftHotkeyPractice/hotbar.tsx
@@ -0,0 +1,21 @@
+import Key from "./slot";
+import "./hotkey.css";
+
+function hotbar() {
+  return (
+    <div className="HotbarContainer">
+      <Key />
+      <Key />
+      <Key />
+      <Key />
+      <Key />
+      <Key />
+      <Key />
+      <Key />
+      <Key />
+      <Key />
+    </div>
+  );
+}
+
+export default hotbar;
