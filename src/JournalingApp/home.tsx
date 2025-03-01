36a1925f7da609ccc9bb7f8a4952bc88797b2e23
Evan <evan.j.ducas@gmail.com>
2025-03-01 10:30:13 -0500

    Add Google OAuth integration and update dependencies; include .env in .gitignore

--- src/JournalingApp/home.tsx
+++ src/JournalingApp/home.tsx
@@ -4,6 +4,16 @@ import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 const Home: React.FC = () => {
   const handleLoginSuccess = (credentialResponse: any) => {
     console.log("Login Success:", credentialResponse);
+
+    const idToken = credentialResponse.credential;
+
+    fetch("http://localhost:3000/auth/google", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id_token: idToken }),
+    }).catch((error) => console.error("failed git gud:", error));
   };
 
   const handleLoginFailure = () => {
@@ -11,7 +21,7 @@ const Home: React.FC = () => {
   };
 
   return (
-    <GoogleOAuthProvider clientId="169031639605-7nkqs7vod1cpfqs1i57k6v0h8thj058i.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
       <div>
         <h1>Hello World</h1>
         <GoogleLogin
@@ -22,5 +32,6 @@ const Home: React.FC = () => {
     </GoogleOAuthProvider>
   );
 };
+console.log("Google Client ID:", import.meta.env.VITE_GOOGLE_CLIENT_ID);
 
 export default Home;
